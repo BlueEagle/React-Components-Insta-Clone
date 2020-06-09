46aa318468d1356f232dc15ccfaa6fc9f5e0202f
Collin Ballou <BlueEagle@users.noreply.github.com>
2020-06-09 14:26:58 -0600

    wip

--- src/components/PostsContainer/Post.js
+++ src/components/PostsContainer/Post.js
@@ -1,5 +1,5 @@
 // You will add code in this file
-import React from "react";
+import React, { useState } from "react";
 import CommentSection from "../CommentSection/CommentSectionContainer";
 import LikeSection from "./LikeSection";
 import PostHeader from "./PostHeader";
@@ -9,6 +9,18 @@ import "./Posts.css";
 // pass props in this file to
 const Post = props => {
   // set up state for the likes
+  const [like, setLike] = useState(false)
+
+  const likeFunction = () => {
+    // debugger
+    if(!like) {
+      setLike(true)
+      props.post.likes += 1
+    } else {
+      setLike(false)
+      props.post.likes -= 1
+    }
+  }
 
   return (
     <div className="post-border">
@@ -25,7 +37,7 @@ const Post = props => {
           src={props.post.imageUrl}
         />
       </div>
-      <LikeSection />
+      <LikeSection likeFunction={likeFunction} props={props.post} />
       <CommentSection
         postId={props.post.imageUrl}
         comments={props.post.comments}
